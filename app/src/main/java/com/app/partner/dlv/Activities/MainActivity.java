8675c91fa8893c87d2878f0e5ac8116c146df192
adriantm26 <adriantorresm1997@gmail.com>
2020-05-06 12:42:49 -0500

    first commit

---
+++ app/src/main/java/com/app/partner/dlv/Activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.app.partner.dlv.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.app.partner.dlv.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
